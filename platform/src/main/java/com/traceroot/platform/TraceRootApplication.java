ad0d61726f80becfedadc231007ef0edbe2240ff
Ozioma Aniagu <buenasnoticas231@gmail.com>
2026-04-16 20:29:06 -0400

    Restructure repo into traceroot-platform and failure-lab modules

--- platform/src/main/java/com/traceroot/platform/TraceRootApplication.java
+++ platform/src/main/java/com/traceroot/platform/TraceRootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PlatformApplication {
+public class TraceRootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PlatformApplication.class, args);
+		SpringApplication.run(TraceRootApplication.class, args);
 	}
 
 }

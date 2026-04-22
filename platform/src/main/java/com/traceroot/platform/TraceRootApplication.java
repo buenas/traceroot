934296bfb7af3f4e66098e8b83d954e2c25fa024
Ozioma Aniagu <buenasnoticas231@gmail.com>
2026-04-22 17:12:41 -0400

    feat(platform): enable Spring Retry application-wide

--- platform/src/main/java/com/traceroot/platform/TraceRootApplication.java
+++ platform/src/main/java/com/traceroot/platform/TraceRootApplication.java
@@ -2,8 +2,10 @@ package com.traceroot.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class TraceRootApplication {
 
 	public static void main(String[] args) {
